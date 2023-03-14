3f4179bfbcf1e33af2505b46c0e5c19e0063f597
akaryaka <a.karyaka.2dit@gmail.com>
2023-03-15 01:12:59 +0600

    refactoring file structure, add icomoon, desktop version complete

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,8 +1,35 @@
-import ymaps from 'ymaps';
+// import ymaps from 'ymaps';
 
-ymaps.load('https://api-maps.yandex.ru/2.1/?lang=en_US').then(maps => {
- const map = new maps.Map('map', {
-    center: [-8.369326, 115.166023],
-    zoom: 7
-  });
-});
+// ymaps.ready(init);
+
+// function init() {
+//   const map = new maps.Map('map', {
+//     center: [54.989347, 73.368221],
+//     zoom: 7,
+//   }),
+//   myGeoObject = new ymaps.GeoObject({
+//     geometry: {
+//       type: "Point",
+//       coordinates: [55.052144, 73.316262]
+//     },
+//   })
+// };
+// ymaps.load('https://api-maps.yandex.ru/2.1/?lang=en_US').then(maps => {
+//   const map = new maps.Map('map', {
+//     center: [54.989347, 73.368221],
+//     zoom: 7,
+//   }),
+//   myGeoObject = new ymaps.GeoObject({
+//     geometry: {
+//       type: "Point",
+//       coordinates: [55.052144, 73.316262]
+//     },
+//     properties: {
+//       // Контент метки.
+//       iconContent: 'Я тащусь',
+//       hintContent: 'Ну давай уже тащи'
+//     }
+//   })
+  
+//   /*55.052144, 73.316262*/ 
+// });
